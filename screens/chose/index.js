feb46a94add855b6e22eb98662a5e86cd096ee5b
iamalinski <gandalfcho@gmail.com>
2024-03-07 23:08:12 +0200

    chose screen styled

--- screens/chose/index.js
+++ screens/chose/index.js
@@ -1,5 +1,15 @@
-import { Text, View, Button, SafeAreaView } from "react-native"
+import { Text, View, SafeAreaView, Pressable, Image } from "react-native"
 import { useNavigation } from "@react-navigation/native"
+
+//images
+import leftArrowIcon from "../../assets/chose/icons/left-arrow.png"
+import rightArrowIcon from "../../assets/chose/icons/right-arrow.png"
+import choseImage from "../../assets/chose/chose.png"
+import bottomLeftShapeImage from "../../assets/chose/bottom-left-shape.png"
+import topRightShapeImage from "../../assets/chose/top-right-shape.png"
+
+//styles
+import core from "../../styles/core"
 
 function Chose() {
   const navigation = useNavigation()
@@ -16,41 +26,129 @@ function Chose() {
         alignItems: "center",
       }}
     >
-      <Text
+      <Image
+        source={choseImage}
         style={{
-          fontSize: 30,
-          fontWeight: "600",
-          textAlign: "center",
-          color: "#2c2c2c",
-          marginBottom: 30,
+          width: 200,
+          height: 145,
+        }}
+        resizeMode="contain"
+      />
+      <Image
+        source={topRightShapeImage}
+        style={{
+          width: 400,
+          height: 400,
+          position: "absolute",
+          top: -50,
+          right: -50,
+          opacity: 0.1,
+          zIndex: 0,
+        }}
+        resizeMode="contain"
+      />
+      <Image
+        source={bottomLeftShapeImage}
+        style={{
+          width: 400,
+          height: 400,
+          position: "absolute",
+          bottom: -50,
+          left: -50,
+          opacity: 0.1,
+          zIndex: 0,
+        }}
+        resizeMode="contain"
+      />
+      <View
+        style={{
+          paddingBottom: 31,
+          marginBottom: 10,
+          alignItems: "center",
         }}
       >
-        Искам да
-      </Text>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "700",
+            textAlign: "center",
+            color: core.primaryColor,
+          }}
+        >
+          Искам да
+        </Text>
+        <View
+          style={{
+            position: "absolute",
+            flexDirection: "row",
+            bottom: 0,
+            gap: 170,
+          }}
+        >
+          <Image
+            source={leftArrowIcon}
+            style={{
+              width: 63,
+              height: 49,
+            }}
+            resizeMode="contain"
+          />
+          <Image
+            source={rightArrowIcon}
+            style={{
+              width: 63,
+              height: 49,
+            }}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
       <View
         style={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-evenly",
-          width: 300,
-          maxWidth: '100%',
+          width: "100%",
+          gap: 10,
+          paddingHorizontal: 15,
+          marginBottom: 15,
         }}
       >
-        <Button
-          title="Се обучавам"
+        <Pressable
           onPress={() => {
             handleNavigate("learn")
           }}
-          color="#2c2c2c"
-        />
-        <Button
-          title="Комуникирам"
+          style={{
+            ...core.primaryButton,
+            flex: 1,
+          }}
+        >
+          <Text style={core.primaryButtonText}>Се обучавам</Text>
+        </Pressable>
+        <Pressable
           onPress={() => {
             handleNavigate("translate")
           }}
-          color="#2c2c2c"
-        />
+          style={{
+            ...core.primaryButton,
+            flex: 1,
+          }}
+        >
+          <Text style={core.primaryButtonText}>Комуникирам</Text>
+        </Pressable>
       </View>
+      <Text
+        style={{
+          fontSize: 13,
+          fontWeight: "400",
+          textAlign: "center",
+          color: core.primaryColor,
+          marginBottom: 30,
+        }}
+      >
+        Вашият избор ще определи направлението, в което искате да получите
+        насока.
+      </Text>
     </SafeAreaView>
   )
 }
